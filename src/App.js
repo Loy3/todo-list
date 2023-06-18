e2ee4a4a8f7e8eeb7ec383bf15962c2341fc03eb
Loy3 <netshiozwiloy@gmail.com>
2023-06-18 19:41:46 +0200

    18/06/2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
+import HomePage from './Components/HomePage';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const signedIn = localStorage.getItem("userStatus");
+  let userStat = false;
+  if (signedIn === '' || signedIn === null) {
+    localStorage.setItem('userStatus', JSON.stringify(false))
+  } else {
+    userStat = JSON.parse(signedIn);
+
+  }
+
+  const [isSignedIn, setSignIn] = useState(userStat);
+  const status = localStorage.getItem("user");
+  console.log(isSignedIn);
+  // const userStatus = () => {
+  //   setSignIn((isSignedIn) => [isSignedIn]);
+  //   console.log(isSignedIn);
+
+  // }
+  // useEffect(() => {
+  //   setSignIn(true)
+  // }, [isSignedIn])
+
+  let lists = "";
+  const stringifiedlist = localStorage.getItem('lists');
+  if (stringifiedlist === "" || stringifiedlist === null) {
+    localStorage.setItem('lists', JSON.stringify([]));
+  } else {
+    lists = JSON.parse(stringifiedlist);
+  }
+
+
+  const [newList, setList] = useState(lists);
+
+  const list = (task, taskDescipt, taskPriority, taskDueDate, userEmail) => {
+    setList((newList) => [...newList, { task: task, taskDescipt: taskDescipt, taskPriority: taskPriority, taskDueDate: taskDueDate, userEmail: userEmail }]);
+    console.log(newList);
+
+  }
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(newList))
+  }, [newList])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={isSignedIn ? <HomePage addNewList={list} /> : <SignIn setSignIn={setSignIn} />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/home' element={<HomePage addNewList={list}/>} />
+{/* <Route path='/home' element={<HomePage addNewList={list}/>} /> */}
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
+export default function NavBar() {
+
+    const navigate = useNavigate();
+
+    function toHome() {
+        navigate("/home");
+    }
+
+
+    function toAdd() {
+        navigate("/add");
+    }
+
+    function signOut() {
+        localStorage.setItem('userStatus', JSON.stringify(false));
+        navigate("/");
+        window.location.reload();
+    }
+
+    let allUsers = [];
+    const stringifiedUser = localStorage.getItem('user');
+    if (stringifiedUser === "" || stringifiedUser === null) {
+        localStorage.setItem('user', JSON.stringify([]));
+        // navigate("/");
+    } else {
+        allUsers = JSON.parse(stringifiedUser);
+        allUsers = [allUsers];
+    }
+
+    return (
+        <nav>
+            <div className='nav-wrap'>
+
+                <div className="topnav" id="myTopnav">
+                    <div className="row">
+                        <div className='column1'>
+                            <div className="topnav-left">
+                                <h1 id={"logo"}>Hello {allUsers[0].userFirstName}</h1>
+                            </div>
+                        </div>
+                        <div className='column3'>
+                            <div className="topnav-right">
+                                <button onClick={signOut}>Sign Out</button>
+                            </div>
+                        </div>
+                        <div className='column2'>
+                            <div className="topnav-center" id="topnav-center" align="center">
+                                <a href='' onClick={toHome}>Home</a>
+                                <a href='' onClick={toAdd}>Add a to-do</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </nav >
+    )
+}
